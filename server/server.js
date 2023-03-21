389b79e5a7a0b010aaa7f0c804ed365e173a1f3f
Felipe Ocampo <felipe.aocampo@gmail.com>
2023-03-21 13:27:58 -0400

    adds btn to test an endpoint on port 4000

--- server/server.js
+++ server/server.js
@@ -1,12 +1,11 @@
 // const express = require("express")
 import express from 'express';
 
-const PORT = 3000,
+const PORT = 8080,
+  app = express();
 
-app = express();
-
-app.get("/api/", (req, res) => {
-  res.send("hello");
+app.get('/api/', (req, res) => {
+  res.send('hello');
 });
 
 app.listen(PORT, () => console.log(`start listening on port : ${PORT}`));
