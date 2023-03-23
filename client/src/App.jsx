e77457c941ab8afa83f001aa38c0721e09a084bc
Felipe Ocampo <felipe.aocampo@gmail.com>
2023-03-23 19:29:23 -0400

    include algo that parses through received introspection schema and returns a visualizer schema that will be used to render visualizer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,20 +4,25 @@ import { EndpointInput } from './components/EndpointInput';
 import { buildClientSchema, getIntrospectionQuery, printSchema } from 'graphql';
 import Editor from './Editor';
 
+import parseReceivedSchema from './funcs/parseIntrospectionQueryResponse';
+
 // backend endpoint: /api/
 
 //graphql tests endpoint: http://localhost:4000/
 
 const App = () => {
   // state
-  const [endpoint, setEndpoint] = useState('https://swapi-graphql.netlify.app/.netlify/functions/index');
+  const [endpoint, setEndpoint] = useState(
+    'https://swapi-graphql.netlify.app/.netlify/functions/index'
+  );
   const [schema, setSchema] = useState(null);
 
   const fetchSchema = async () => {
     // introspection query to get schema
     const schema = await request(endpoint, getIntrospectionQuery());
-    console.log('schema: ', schema);
-    setSchema(schema);
+
+    const parsedSchemaData = parseReceivedSchema(schema);
+    setSchema(parsedSchemaData);
 
     // format schema for CodeMirror hint and lint
     // const clientSchema = buildClientSchema(schema);
@@ -27,12 +32,13 @@ const App = () => {
     // const schemaSDL = printSchema(clientSchema);
     // console.log('schemaSDL: ', schemaSDL);
   };
+  console.log(schema);
 
   return (
     <main>
       <Editor schema={schema} endpoint={endpoint}></Editor>
       <section className="endpoint-section">
-        <EndpointInput endpoint={endpoint} setEndpoint={setEndpoint}/>
+        <EndpointInput endpoint={endpoint} setEndpoint={setEndpoint} />
         <button onClick={fetchSchema}>Fetch Schema</button>
       </section>
     </main>
