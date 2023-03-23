de59a1d98c7433cb20f59e6d0c8a323b255873b1
Cole Bryan <CNBryan129@gmail.com>
2023-03-22 21:16:42 -0700

    played around with React Flow

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,37 @@
-import React from 'react';
+import React, {useCallback} from 'react';
+import ReactFlow, { 
+  MiniMap,
+  Controls,
+  Background,
+  useNodesState, 
+  useEdgesState, 
+  addEdge 
+} from 'reactflow'; //added for ReactFlow
+
+import 'reactflow/dist/style.css'; //added for ReactFlow
 
 import { request, gql } from 'graphql-request';
+
+// ************ReactFlow Playground****************
+
+
+
+
+
+const initialNodes = [
+  { id: '1', position: { x: 0, y: 0 }, data: { label: '1' } },
+  { id: '2', position: { x: 0, y: 100 }, data: { label: '2' } },
+];
+const initialEdges = [{ id: 'e1-2', source: '1', target: '2' }];
+
+
+
+
+
+
+
+// ************************************************
+
 
 // backend endpoint: /api/
 
@@ -139,12 +170,39 @@ const App = () => {
     console.log(schema);
   };
 
+  //Commented out to make room for ReactFlow
+  // return (
+  //   <div>
+  //     <h1>OSS: Our app</h1>
+  //     <button onClick={testGql}>Send gql request</button>
+  //   </div>
+  // );
+
+  //*** React Playground *** *//
+
+  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+
+  const onConnect = useCallback((params) => setEdges((eds) => addEdge(params, eds)), [setEdges]);
+
+  //*** **************** *** *//
+
   return (
-    <div>
-      <h1>OSS: Our app</h1>
-      <button onClick={testGql}>Send gql request</button>
+    <div style={{ width: '100vw', height: '100vh', background: 'beige'}}>
+      <ReactFlow 
+        nodes={nodes}
+        edges={edges}
+        onNodesChange={onNodesChange}
+        onEdgesChange={onEdgesChange}
+        onConnect={onConnect}
+      >
+        <Controls />
+        <MiniMap />
+        <Background variant="dots" gap={12} size={1}/>
+        </ReactFlow>
     </div>
   );
+
 };
 
 export default App;
